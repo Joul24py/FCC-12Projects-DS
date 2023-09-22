0e6bb6090c603f44f9313f8bc7bbe378c86552e3
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2023-09-22 09:38:45 -0600

    Improved project 1

--- 01-SimpleStockPrice/main.py
+++ 01-SimpleStockPrice/main.py
@@ -1,16 +1,59 @@
 import yfinance as yf
 import streamlit as st
+import pandas as pd
 
 st.write("""
 # Simple Stock Price App
 
-Shown are the stock closing price and volume of Google
+Shown are the stock closing price and volume of:
+
+- Google
+- Apple
+- Microsoft
+- IBM
+- Amazon
 
 """)
 
+# Google
 tickerSymbol = 'GOOGL'
 tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(start = '2010-05-31', end = '2023-09-20')
 
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
+closeDf = tickerDf.Close.rename('Google')
+volumeDf = tickerDf.Volume.rename('Google')
+
+# Apple
+tickerSymbol = 'AAPL'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(start = '2010-05-31', end = '2023-09-20')
+
+closeDf = pd.concat([closeDf, tickerDf.Close.rename('Apple')], axis = 1)
+volumeDf = pd.concat([volumeDf, tickerDf.Volume.rename('Apple')], axis = 1)
+
+# Microsoft
+tickerSymbol = 'MSFT'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(start = '2010-05-31', end = '2023-09-20')
+
+closeDf = pd.concat([closeDf, tickerDf.Close.rename('Microsoft')], axis = 1)
+volumeDf = pd.concat([volumeDf, tickerDf.Volume.rename('Microsoft')], axis = 1)
+
+# IBM
+tickerSymbol = 'IBM'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(start = '2010-05-31', end = '2023-09-20')
+
+closeDf = pd.concat([closeDf, tickerDf.Close.rename('IBM')], axis = 1)
+volumeDf = pd.concat([volumeDf, tickerDf.Volume.rename('IBM')], axis = 1)
+
+# Amazon
+tickerSymbol = 'AMZN'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(start = '2010-05-31', end = '2023-09-20')
+
+closeDf = pd.concat([closeDf, tickerDf.Close.rename('Amazon')], axis = 1)
+volumeDf = pd.concat([volumeDf, tickerDf.Volume.rename('Amazon')], axis = 1)
+
+st.line_chart(closeDf)
+st.line_chart(volumeDf)
