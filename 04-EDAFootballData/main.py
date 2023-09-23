db1872079fbfcff360dbf995bfa89fbaa46d600b
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2023-09-22 20:41:21 -0600

    Done project 4

--- 04-EDAFootballData/main.py
+++ 04-EDAFootballData/main.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-st.title('NBA Player Stats Explorer')
+st.title('NFL Football Stats (Rushing) Explorer')
 
 st.markdown("""
-This app performs simple web scrapping of NBA player stats data
+This app performs simple web scrapping of NFL football player stats data
 
 Python libraries:
 
@@ -16,52 +16,52 @@ Python libraries:
 - ```pandas```
 - ```streamlit```
 
-Data source: [Basketball Reference](https://www.basketball-reference.com/)
+Data source: [Pro Football Reference](https://www.pro-football-reference.com/)
 """)
 
 st.sidebar.header('User Input Features')
-selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950, 2024))))
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1990, 2023))))
 
 def load_data(year):
-    url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
-    html = pd.read_html(url, header = 0)
+    url = 'https://www.pro-football-reference.com/years/' + str(year) + '/rushing.htm'
+    html = pd.read_html(url, header = 1)
     df = html[0]
     raw = df.drop(df[df.Age == 'Age'].index)
     raw = raw.fillna(0)
-    playerstats = raw.drop(['Rk'], axis = 1)
+    playerstats = raw.drop(['Rk'], axis=1)
     return playerstats
 playerstats = load_data(selected_year)
 
 sorted_unique_team = sorted(playerstats.Tm.unique())
 selected_team = st.sidebar.multiselect('Team', sorted_unique_team)
 
-unique_pos = ['PG', 'SG', 'PF', 'SF', 'C']
+unique_pos = ['RB','QB','WR','FB','TE']
 selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
 
-df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & playerstats.Pos.isin(selected_pos)]
+df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
 
 st.header('Display player stats of selected team(s)')
-st.write('Data dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + 'columns')
+st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns')
 st.dataframe(df_selected_team)
 
 def fileDownload(df):
-    csv = df.to_csv(index = False)
-    b64 = base64.b64encode(csv.encode()).decode()
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
     href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
     return href
 
-st.markdown(fileDownload(df_selected_team), unsafe_allow_html = True)
+st.markdown(fileDownload(df_selected_team), unsafe_allow_html=True)
 
-if st.button('Intercorrelation Heatmap'):
+if st.button('Intercorrelation heatmap'):
     st.header('Intercorrelation matrix heatmap')
-    df_selected_team.to_csv('output.csv', index = False)
+    df_selected_team.to_csv('output.csv',index=False)
     df = pd.read_csv('output.csv')
     df = df.drop(['Player', 'Pos', 'Tm'], axis = 1)
-    
+
     corr = df.corr()
     mask = np.zeros_like(corr)
     mask[np.triu_indices_from(mask)] = True
     with sns.axes_style("white"):
-        f, ax = plt.subplots(figsize = (7, 5))
-        ax = sns.heatmap(corr, mask = mask, vmax = 1, square = True, cmap = 'RdYlGn')
+        f, ax = plt.subplots(figsize=(7, 5))
+        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True, cmap = 'RdYlGn')
     st.pyplot(f)
